683a68ecbb2ea66b31e2db686fe5f2027e058ae5
Joe Scott <jbs198585@gmail.com>
2022-03-17 14:13:29 -0400

    req and res objects

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
   console.log("WE GOT A NEW REQUEST!!")
+  // res.send("HELLO, WE GOT YOUR REQUEST! THIS IS A RESPONSE!")
+  res.send('<h1>This is my webpage!</h1>')
 })
 
 app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000!");
+  console.log("LISTENING ON PORT 3000");
 });
